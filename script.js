56461d6fb424d7ce12599d585b5edf238d6d9019
OlgaEva <olga.rosas@gmail.com>
2020-05-21 15:26:16 -0400

    add deleteMe fn

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ function newTodo() {
   const addTodo = document.createElement("li")
   const liText = document.getElementById("input").value
     if(liText == ''){
-      alert("You cannot submit an empty ToDo!")
+      alert("You cannot submit an empty TODO!")
     }else{
-    addTodo.innerHTML = `<label><input type='checkbox' id='checkbox'>&ensp;${liText}<span>&ensp;<button type="button">Delete</button></span></label>`
+    addTodo.innerHTML = `<label><input type='checkbox' id='checkbox'>&ensp;${liText}<span>&ensp;<button type="button" onClick="deleteMe()">Delete</button></span></label>`
 
       list.append(addTodo)
       document.getElementById("input").value = ""
@@ -30,31 +30,38 @@ function newTodo() {
       uncheckedCount.innerText = `${uncheckedCounter}`
   }
 
-addTodo.addEventListener("click", function(event){
-  dealWithChecked()
-})
-
+  addTodo.addEventListener("click", function(event){
+    dealWithChecked()
+    })
 }
 
-function dealWithChecked() {
-  const checkboxes = document.querySelectorAll("li")
+// function dealWithChecked() {
+//   const checkboxes = document.querySelectorAll("li")
 
-  checkboxes.forEach(function(li) {
-    if(li.children[0].childNodes[0].checked == true){
-      console.log(li.children[0].childNodes[0].checked)
-      li.classList.add("checked")
-      console.log(uncheckedCounter)
-      uncheckedCounter--;
-      uncheckedCount.innerText = `${uncheckedCounter}`
-      console.log(uncheckedCounter)
-    } else{
-      console.log(li.children[0].childNodes[0].checked)
-      li.classList.remove("checked")
-      // console.log(uncheckedCounter)
-      // uncheckedCounter++;
-      // uncheckedCount.innerText = `${uncheckedCounter}`
-      // console.log(uncheckedCounter)
-    }
-  })
+//   checkboxes.forEach(function(li) {
+//     if(li.children[0].childNodes[0].checked == true){
+//       console.log(li.children[0].childNodes[0].checked)
+//       li.classList.add("checked")
+//       console.log(uncheckedCounter)
+//       uncheckedCounter--;
+//       uncheckedCount.innerText = `${uncheckedCounter}`
+//       console.log(uncheckedCounter)
+//     } else{
+//       console.log(li.children[0].childNodes[0].checked)
+//       li.classList.remove("checked")
+//       // console.log(uncheckedCounter)
+//       // uncheckedCounter++;
+//       // uncheckedCount.innerText = `${uncheckedCounter}`
+//       // console.log(uncheckedCounter)
+//     }
+//   })
+// }
+
+function deleteMe(){
+  event.target.parentElement.parentElement.remove();
+  counter--;
+  itemCount.innerText = `${counter}`
+  uncheckedCounter--;
+  uncheckedCount.innerText = `${uncheckedCounter}`
 }
 
